3c2910919bde4fcee5b650a1841317ce28da87d5
maki <maki111000664455@gmail.com>
2025-01-27 17:53:04 +0900

    多分勝った

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use std::{fs::File, io::Write};
 
-mod generate_kana_table;
-
-use generate_kana_table::{deserealizer::azik_deserializer, gen_kana_table::gen_hiragana_table};
-
-pub use crate::generate_kana_table::{azik_config, deserealizer, gen_kana_table};
+use generate_kana_table::{azik_deserializer, gen_hiragana_table};
 
 fn main() {
     let config = azik_deserializer();
